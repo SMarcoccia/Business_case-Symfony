0d59ad823ea22687359be522e50a16ba6600195c
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-05-08 11:23:46 +0200

    ras

--- src/Controller/Api/V1/AnnonceController.php
+++ src/Controller/Api/V1/AnnonceController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 /**
  * @Route("/api/v1", name="api_v1_")
  */
